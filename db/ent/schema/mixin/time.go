d48d2159f6867b7a9cab5d713c0697300e8f15f7
Toby Yan <me@tobyan.com>
2024-07-25 13:59:32 +0800

    feat: add soft delete inteceptors
    Signed-off-by: Toby Yan <me@tobyan.com>

--- db/ent/schema/mixin/time.go
+++ db/ent/schema/mixin/time.go
@@ -1,9 +1,12 @@
 package mixin
 
 import (
+	"context"
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/sql"
+	"fmt"
 	"time"
 
-	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
 	"entgo.io/ent/schema/mixin"
 )
@@ -25,4 +28,72 @@ func (TimeMixin) Fields() []ent.Field {
 		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now).Comment("更新时间"),
 		field.Time("deleted_at").Optional().Nillable().Comment("删除时间"),
 	}
+}
+
+type withSoftDeletedKey struct{}
+
+// WithSoftDeleted returns a new context that skips the soft-delete interceptor/mutators.
+func WithSoftDeleted(parent context.Context) context.Context {
+	return context.WithValue(parent, withSoftDeletedKey{}, true)
+}
+
+// Interceptors of the SoftDeleteMixin.
+func (d TimeMixin) Interceptors() []ent.Interceptor {
+	return []ent.Interceptor{
+		ent.TraverseFunc(func(ctx context.Context, q ent.Query) error {
+			// With soft-deleted, means include soft-deleted entities.
+			if skip, _ := ctx.Value(withSoftDeletedKey{}).(bool); skip {
+				return nil
+			}
+
+			qp, ok := q.(interface {
+				WhereP(...func(*sql.Selector))
+			})
+			if !ok {
+				return fmt.Errorf("unexpected query type %T", q)
+			}
+			d.P(qp)
+			return nil
+		}),
+	}
+}
+
+// Hooks of the SoftDeleteMixin.
+func (d TimeMixin) Hooks() []ent.Hook {
+	return []ent.Hook{
+
+		func(next ent.Mutator) ent.Mutator {
+			return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
+				// Skip soft-delete, means delete the entity permanently.
+				if skip, _ := ctx.Value(withSoftDeletedKey{}).(bool); skip {
+					return next.Mutate(ctx, m)
+				}
+
+				// check if delete op
+				if !m.Op().Is(ent.OpDelete | ent.OpDeleteOne) {
+					return next.Mutate(ctx, m)
+				}
+
+				mx, ok := m.(interface {
+					SetOp(ent.Op)
+					SetDeletedAt(time.Time)
+					WhereP(...func(*sql.Selector))
+				})
+				if !ok {
+					return nil, fmt.Errorf("unexpected mutation type %T", m)
+				}
+				d.P(mx)
+				mx.SetOp(ent.OpUpdate)
+				mx.SetDeletedAt(time.Now())
+				return next.Mutate(ctx, m)
+			})
+		},
+	}
+}
+
+// P adds a storage-level predicate to the queries and mutations.
+func (d TimeMixin) P(w interface{ WhereP(...func(*sql.Selector)) }) {
+	w.WhereP(
+		sql.FieldIsNull("deleted_at"),
+	)
 }
