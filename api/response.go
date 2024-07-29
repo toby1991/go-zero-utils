a975983d761fc88ab8f9106be023cb1ee1e2be2e
Toby Yan <me@tobyan.com>
2024-07-29 13:22:12 +0800

    feat: process grpc error
    Signed-off-by: Toby Yan <me@tobyan.com>

--- api/response.go
+++ api/response.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"net/http"
 
 	"github.com/zeromicro/go-zero/rest/httpx"
@@ -14,6 +16,13 @@ type Body struct {
 
 func Response(w http.ResponseWriter, resp interface{}, err error) {
 	if err != nil {
+
+		// grpc return process
+		if grpcResponse(w, err) {
+			return
+		}
+
+		// default
 		httpx.WriteJson(w, http.StatusUnprocessableEntity, &Body{
 			Code: -1,
 			Msg:  err.Error(),
@@ -22,10 +31,51 @@ func Response(w http.ResponseWriter, resp interface{}, err error) {
 		return
 	}
 
+	// success
 	httpx.OkJson(w, &Body{
 		Code: 0,
 		Msg:  "OK",
 		Data: resp,
 	})
 	return
+}
+
+// process grpc response, if processed, return true
+func grpcResponse(w http.ResponseWriter, err error) bool {
+	// no error
+	if err == nil {
+		return false
+	}
+
+	ev, ok := status.FromError(err)
+	// not grpc error
+	if !ok {
+		return false
+	}
+
+	code := ev.Code()
+	switch code {
+	case codes.NotFound:
+		httpx.WriteJson(w, http.StatusNotFound, &Body{
+			Code: -1,
+			Msg:  err.Error(),
+			Data: nil,
+		})
+		return true
+	case codes.PermissionDenied:
+		httpx.WriteJson(w, http.StatusForbidden, &Body{
+			Code: -1,
+			Msg:  err.Error(),
+			Data: nil,
+		})
+		return true
+	default:
+		// default
+		httpx.WriteJson(w, http.StatusUnprocessableEntity, &Body{
+			Code: -1,
+			Msg:  err.Error(),
+			Data: nil,
+		})
+		return true
+	}
 }
