0b8fda2586d1f0fee895e3d1097eb53e099f4de3
Toby Yan <me@tobyan.com>
2025-03-13 12:21:30 +0800

    fix: change faktory interface
    Signed-off-by: Toby Yan <me@tobyan.com>

--- faktory/interface.go
+++ faktory/interface.go
@@ -3,6 +3,7 @@ package faktory
 import (
 	"context"
 	faktory "github.com/contribsys/faktory/client"
+	"github.com/toby1991/go-zero-utils/queue"
 	"github.com/zeromicro/go-zero/core/service"
 )
 
@@ -10,6 +11,6 @@ type FaktoryClient interface {
 	service.Service
 
 	Context() context.Context
-	SetProcessor(jobNameProcessorMap map[string]JobProcessor)
+	SetProcessor(jobNameProcessorMap map[string]queue.JobProcessor)
 	Push(job *faktory.Job) error
 }
